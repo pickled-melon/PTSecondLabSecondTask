a71c08673afb9bb8c018a0ad560b5ff408c7957b
Daniil Knaub <andy.typing@mail.ru>
2026-03-03 12:51:41 +0800

    Added the Logic class

--- Form1.cs
+++ Form1.cs
@@ -7,4 +7,36 @@ namespace PTSecondLabSecondTask
             InitializeComponent();
         }
     }
+
+    class Logic
+    {
+        public static int GetSumOfNumbersInText(string inputText)
+        {
+            int currentNumber = 0;
+            char currentSign = '+';
+
+            int numberMultiplier;
+            int textNumbersSum = 0;
+
+            foreach (char c in inputText)
+            {
+                if (c == '+' || c == '-')
+                {
+                    numberMultiplier = (currentSign == '-') ? -1 : 1;
+                    textNumbersSum += numberMultiplier * currentNumber;
+                    currentNumber = 0;
+                    currentSign = c;
+                }
+                else if (c >= '0' && c <= '9')
+                {
+                    currentNumber = currentNumber * 10 + (c - '0');
+                }
+            }
+
+            numberMultiplier = (currentSign == '-') ? -1 : 1;
+            textNumbersSum += numberMultiplier * currentNumber;
+
+            return textNumbersSum;
+        }
+    }
 }
